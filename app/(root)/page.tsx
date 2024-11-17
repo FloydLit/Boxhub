352794d01f7a2f456536817a4b23c762f25a04e7
FloydLit <lithuge@gmail.com>
2024-11-17 03:46:38 +0200

    Auth Config, Dashboard, Nav & Sidebar and upload modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">
+    <div className="flex-center h-screen">
+      <h1 className="h1">
         BoxHub - The only storage you need
       </h1>
     </div>
