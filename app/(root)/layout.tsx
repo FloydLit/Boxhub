182edc518513ff3ee6a56cbd65c8129d0dedabf6
FloydLit <lithuge@gmail.com>
2024-11-17 17:19:02 +0200

    File upload Functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import Sidebar from "@/components/Sidebar";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import React from "react";
+import { Toaster } from "@/components/ui/toaster";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
     const currentUser = await getCurrentUser();
@@ -13,12 +14,12 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
     return <main className="flex h-screen">
         <Sidebar {... currentUser}/>
         <section className="flex h-full flex-1 flex-col">
-            <MobileNavigation /> 
-            <Header />
+            <MobileNavigation {...currentUser}/> 
+            <Header userId={currentUser.$id} accountId={currentUser.accountId} />
             <div className="main-content">{children}</div>
         </section>
 
-
+        <Toaster />
     </main>
 };
 
