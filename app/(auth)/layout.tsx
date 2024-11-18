ac3ca02dbd798b639bf57cfda2d7cb5636a33948
FloydLit <lithuge@gmail.com>
2024-11-18 18:19:49 +0200

    globals.css changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
         <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/boxhub.png"
             alt="logo"
             width={224}
             height={82}
@@ -15,9 +15,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           />
 
           <div className="space-y-5 text-white">
-            <h1 className="h1">Manage your files the best way</h1>
+            <h1 className="h1">Store Smart, Access Anywhere:</h1>
             <p className="body-1">
-              This is a place where you can store all your documents.
+              Your Data, Simplified.
             </p>
           </div>
           <Image
@@ -33,7 +33,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            src="/assets/icons/logo-full-brand.svg"
+            src="/assets/icons/boxhub.png"
             alt="logo"
             width={224}
             height={82}
